4efb3b8e4763c71ab9fba66b3d706ed1c808efcb
Kamil <millos10@wp.pl>
2022-01-23 20:49:24 +0100

    Instruction details

--- backend/views/instruction/view.php
+++ backend/views/instruction/view.php
@@ -2,29 +2,32 @@
 
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use rmrevin\yii\fontawesome\FA;
+
+rmrevin\yii\fontawesome\AssetBundle::register($this);
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Instruction */
 
 $this->title = $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Instructions', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => 'Instrukcje', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="instruction-view">
+    
+        <p>
+            <?= Html::a('Edytuj', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+            <?= Html::a('Usuń', ['delete', 'id' => $model->id], [
+                'class' => 'btn btn-danger',
+                'data' => [
+                    'confirm' => 'Czy na pewno chcesz usunąć tą pozycję?',
+                    'method' => 'post',
+                ],
+            ]) ?>
+        </p>
 
     <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
 
     <?= DetailView::widget([
         'model' => $model,
@@ -37,5 +40,45 @@ $this->params['breadcrumbs'][] = $this->title;
             'additional_info',
         ],
     ]) ?>
-
+        
+    <div class="card mb-3">
+        <div class="card-header"><h4><?= FA::icon("list") ?> Charakterystyki</h4></div>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>Nazwa charakterystyki</th>
+                    <th>Wartość wzorcowa</th>
+                    <th>Tol-</th>
+                    <th>Tol+</th>
+                    <th>Jednostka</th>
+                    <th>Liczba powtórzeń</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody class="container-items">
+                <?php foreach ($model->instructionCharacteristics as $i => $modelCharacteristic): ?>
+                    <tr>
+                        <td>
+                            <?= $modelCharacteristic->name ?>
+                        </td>
+                        <td>
+                            <?= $modelCharacteristic->norm_value ?>
+                        </td>
+                        <td>
+                            <?= $modelCharacteristic->add_tolerance ?>
+                        </td>
+                        <td>
+                            <?= $modelCharacteristic->sub_tolerance ?>
+                        </td>
+                        <td>
+                            <?= $modelCharacteristic->measure_unit ?>
+                        </td>
+                        <td>
+                            <?= $modelCharacteristic->number_of_repetition ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </div>
